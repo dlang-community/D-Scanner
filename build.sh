0b8834a1b395ca930e58434d1141f561e7cc3704
Hackerpilot <briancschott@gmail.com>
2013-02-03 16:20:52 +0000

    Small code cleanup. Added ldc build command.

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
-dmd *.d std/d/*.d -release -inline -noboundscheck -O -w -wi -m64 -property -ofdscanner -L-lsqlite3 #-inline
+#dmd *.d std/d/*.d -release -inline -noboundscheck -O -w -wi -m64 -property -ofdscanner -L-lsqlite3 #-inline
 #dmd *.d std/d/*.d -g -m64 -w -wi -property -ofdscanner -L-lsqlite3 #-unittest
+ldc2 -O3 *.d std/d/*.d -of=dscanner -L-lsqlite3
