62c27452ca0a52b29826a29eb6ad8b6b7a391b3d
Hackerpilot <briancschott@gmail.com>
2013-02-08 06:35:41 -0800

    Include string cache changes from @blackwhale

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #dmd *.d std/d/*.d -release -inline -noboundscheck -O -w -wi -m64 -property -ofdscanner -L-lsqlite3 #-inline
 #dmd *.d std/d/*.d -g -m64 -w -wi -property -ofdscanner -unittest
-#ldc2 -O3 *.d std/d/*.d -of=dscanner -release -vectorize -m64
-ldc2 *.d std/d/*.d -of=dscanner -unittest -m64 -g
+ldc2 -O2 *.d std/d/*.d -of=dscanner -release -vectorize -m64
+#ldc2 *.d std/d/*.d -of=dscanner -unittest -m64 -g
