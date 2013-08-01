28e43e21b3078f390b2949d7c102d2350a64f060
Hackerpilot <briancschott@gmail.com>
2013-08-01 20:56:01 +0000

    Fixed dumb error with comment characters

--- editors/textadept/modules/dmd/init.lua
+++ editors/textadept/modules/dmd/init.lua
@@ -1,7 +1,7 @@
-//          Copyright Brian Schott (Sir Alaran) 2012.
-// Distributed under the Boost Software License, Version 1.0.
-//    (See accompanying file LICENSE_1_0.txt or copy at
-//          http://www.boost.org/LICENSE_1_0.txt)
+--          Copyright Brian Schott (Sir Alaran) 2012.
+-- Distributed under the Boost Software License, Version 1.0.
+--    (See accompanying file LICENSE_1_0.txt or copy at
+--          http://www.boost.org/LICENSE_1_0.txt)
 
 local M = {}
 
