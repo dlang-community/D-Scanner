4a7f4b3da41be98d7d9d653f4413b056a51e0607
Hackerpilot <briancschott@gmail.com>
2013-10-02 23:36:01 -0700

    #55 I'm an idiot
    Need to keep the build.sh and build.bat in sync...

--- build.bat
+++ build.bat
@@ -1,1 +1,1 @@
-dmd main.d stats.d imports.d highlighter.d ctags.d astprinter.d outliner.d stdx/d/lexer.d stdx/d/parser.d stdx/d/entities.d stdx/d/ast.d -wi -ofdscanner
+dmd main.d stats.d imports.d highlighter.d ctags.d astprinter.d outliner.d formatter.d stdx/d/lexer.d stdx/d/parser.d stdx/d/entities.d stdx/d/ast.d -wi -ofdscanner
