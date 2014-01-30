575e75fc9184c3e207993256a620408af6368b94
Hackerpilot <briancschott@gmail.com>
2014-01-30 10:45:14 -0800

    Fix #90

--- build.bat
+++ build.bat
@@ -1,1 +1,1 @@
-dmd main.d stats.d imports.d highlighter.d ctags.d astprinter.d formatter.d outliner.d style.d stdx/*.d stdx/d/*.d -ofdscanner.exe -m64 -O -release -noboundscheck
+dmd main.d stats.d imports.d highlighter.d ctags.d astprinter.d formatter.d outliner.d stdx/*.d stdx/d/*.d analysis/*.d -ofdscanner -m64 -g -O -release -noboundscheck -inline
