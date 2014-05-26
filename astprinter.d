d8f4ed230744067be26dfa2d4399f081d673af45
Hackerpilot <briancschott@gmail.com>
2014-05-26 01:23:35 +0000

    Fix AST output for several tokens

--- astprinter.d
+++ astprinter.d
@@ -1318,7 +1318,7 @@ class XMLPrinter : ASTVisitor
 		case tok!"wstringLiteral": tagName = "wstringLiteral"; break;
 		case tok!"scriptLine": tagName = "scriptLine"; break;
 		case tok!"$": output.writeln("<dollar/>"); return;
-		default: tagName = "token"; break;
+		default: output.writeln("<", str(token.type), "/>"); return;
 		}
 		output.writeln("<", tagName, ">", xmlEscape(token.text), "</", tagName, ">");
 	}
