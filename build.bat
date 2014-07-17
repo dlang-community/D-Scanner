7fbc7d0ea5dbb12afa252c12682a81b3bbc82281
Hackerpilot <briancschott@gmail.com>
2014-07-17 03:28:21 -0700

    Fix Windows build

--- build.bat
+++ build.bat
@@ -9,8 +9,8 @@ set ANALYSIS=
 set INIFILED=
 
 for %%x in (*.d) do set CORE=!CORE! %%x
-for %%x in (std/*.d) do set STD=!STD! std/%%x
-for %%x in (std/d/*.d) do set STDD=!STDD! std/d/%%x
+for %%x in (libdparse/src/std/*.d) do set STD=!STD! libdparse/src/std/%%x
+for %%x in (libdparse/src/std/d/*.d) do set STDD=!STDD! libdparse/src/std/d/%%x
 for %%x in (analysis/*.d) do set ANALYSIS=!ANALYSIS! analysis/%%x
 for %%x in (inifiled/source/*.d) do set INIFILED=!INIFILED! inifiled/source/%%x
 
