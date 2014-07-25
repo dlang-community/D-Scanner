31e9c74b2a70217ba42db49d5cc6e16225b38757
Hackerpilot <briancschott@gmail.com>
2014-07-24 18:17:43 -0700

    Update my super-secret identity

--- analysis/style.d
+++ analysis/style.d
@@ -1,4 +1,4 @@
-//          Copyright Brian Schott (Sir Alaran) 2014.
+//          Copyright Brian Schott (Hackerpilot) 2014.
 // Distributed under the Boost Software License, Version 1.0.
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
--- highlighter.d
+++ highlighter.d
@@ -1,5 +1,4 @@
-
-//          Copyright Brian Schott (Sir Alaran) 2012.
+//          Copyright Brian Schott (Hackerpilot) 2012.
 // Distributed under the Boost Software License, Version 1.0.
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
