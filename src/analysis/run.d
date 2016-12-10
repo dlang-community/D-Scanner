be555a74b8b7a32d2c85543ca04040ef3db33f26
Sebastian Wilzbach <seb@wilzba.ch>
2016-12-10 18:15:11 +0100

    Add ImportSortedness checker

--- src/analysis/run.d
+++ src/analysis/run.d
@@ -60,6 +60,7 @@ import analysis.alias_syntax_check;
 import analysis.static_if_else;
 import analysis.lambda_return_check;
 import analysis.auto_function;
+import analysis.imports_sortedness;
 
 import dsymbol.string_interning : internString;
 import dsymbol.scope_;
@@ -358,6 +359,10 @@ MessageSet analyze(string fileName, const Module m, const StaticAnalysisConfig a
 	if (analysisConfig.auto_function_check != Check.disabled)
 		checks ~= new AutoFunctionChecker(fileName,
 		analysisConfig.auto_function_check == Check.skipTests && !ut);
+
+	if (analysisConfig.imports_sortedness != Check.disabled)
+		checks ~= new ImportSortednessCheck(fileName,
+		analysisConfig.imports_sortedness == Check.skipTests && !ut);
 
 	version (none)
 		if (analysisConfig.redundant_if_check != Check.disabled)
