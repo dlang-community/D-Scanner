097f0340a5979efebe2a8f1ac965cabe44e28162
Basile Burg <basile.b@gmx.com>
2018-05-04 23:07:28 +0200

    Only test DMD beta when matching release is not yet released

--- .travis.sh
+++ .travis.sh
@@ -2,13 +2,26 @@
 
 set -e
 
-if [[ $BUILD == dub ]]; then
-    dub test
-elif [[ $DC == ldc2 ]]; then
-    git submodule update --init --recursive
-    make test DC=ldmd2
-else
-    git submodule update --init --recursive
-    make test
-    make lint
+if [[ $DC == dmd ]]; then
+	wget http://downloads.dlang.org/releases/LATEST
+	latest=`cat LATEST`
+	actual=`dmd --version | grep -Po -m1 v[.0-9\-a-zA-Z]*`
+	actual=${actual:1}
+	nightly=`echo $actual | grep -Po master`
+	if [[ $actual == $latest* ]] && [[ $actual != $latest ]] && [[ -z "$nightly" ]]; then
+		old_beta=1
+	fi
+fi
+
+if [[ -z "$old_beta" ]]; then
+	if [[ $BUILD == dub ]]; then
+		dub test
+	elif [[ $DC == ldc2 ]]; then
+		git submodule update --init --recursive
+		make test DC=ldmd2
+	else
+		git submodule update --init --recursive
+		make test
+		make lint
+	fi
 fi
