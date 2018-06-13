59ce0c03005fedc81e47a1cbcf28698585fd61c0
Hiroki Noda <kubo39@gmail.com>
2018-06-13 09:53:17 +0900

    Fix copyright

--- src/dscanner/analysis/pointless_arithmetic_check.d
+++ src/dscanner/analysis/pointless_arithmetic_check.d
@@ -1,4 +1,4 @@
-//          Copyright Brian Schott (Hackerpilot) 2016.
+//          Copyright Hiroki Noda 2018.
 // Distributed under the Boost Software License, Version 1.0.
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
