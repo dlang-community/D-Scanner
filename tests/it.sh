4033e994fa7120575926a169063730ab68307a96
WebFreak001 <gh@webfreak.org>
2023-07-09 00:15:36 +0200

    it: only rebuild dscanner outside CI
    fix windows redirects

--- tests/it.sh
+++ tests/it.sh
@@ -3,14 +3,17 @@
 set -eu -o pipefail
 
 DSCANNER_DIR="$(dirname -- $( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd ))"
-dub build --root="$DSCANNER_DIR"
+
+if [ -z "${CI:-}" ]; then
+	dub build --root="$DSCANNER_DIR"
+fi
 
 cd "$DSCANNER_DIR/tests"
 
 # IDE APIs
 # --------
 # checking that reporting format stays consistent or only gets extended
-diff <(jq -S . <(../bin/dscanner --report it/source_autofix.d)) <(jq -S . it/source_autofix.report.json)
-diff <(jq -S . <(../bin/dscanner --resolveMessage b16 it/source_autofix.d)) <(jq -S . it/source_autofix.autofix.json)
+diff <(../bin/dscanner --report it/source_autofix.d | jq -S .) <(jq -S . it/source_autofix.report.json)
+diff <(../bin/dscanner --resolveMessage b16 it/source_autofix.d | jq -S .) <(jq -S . it/source_autofix.autofix.json)
 
 
